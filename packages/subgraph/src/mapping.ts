de52bbe624efb2174460486c69e1e503a3701590
azf20 <azfuller20@gmail.com>
2022-02-19 16:11:03 -0700

    Update mapping.ts

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -10,11 +10,9 @@ networks.set("1", "mainnet")
 
 export function handleNewEpochBlock(event: newEpochBlock): void {
 
-/*
-  if(event.transaction.from !== Address.fromString("0xE09750abE36beA8B2236E48C84BB9da7Ef5aA07c")) { // Can this be relied upon?
+  if(event.transaction.from != Address.fromString("0xE09750abE36beA8B2236E48C84BB9da7Ef5aA07c")) { // Can this be relied upon?
     return
   }
-  */
 
   let senderString = event.transaction.from.toHexString()
   let oracle = Oracle.load(senderString);
@@ -25,31 +23,27 @@ export function handleNewEpochBlock(event: newEpochBlock): void {
     oracle.save();
   }
 
-  log.warning('NetworkId: {}', [event.params.networkId.toString()])
-  log.warning('hasNetworkId: {}', [networks.has(event.params.networkId.toString()).toString()])
-
   if(networks.has(event.params.networkId.toString())) {
 
-  let network = Network.load(event.params.networkId.toString());
+    let network = Network.load(event.params.networkId.toString());
 
-  if (network === null) {
-    network = new Network(event.params.networkId.toString());
-    network.name = networks.get(event.params.networkId.toString());
-    network.save();
-  }
+    if (network === null) {
+      network = new Network(event.params.networkId.toString());
+      network.name = networks.get(event.params.networkId.toString());
+      network.save();
+    }
 
-  let epoch = Epoch.load(event.params.epoch.toString());
+    let epoch = Epoch.load(event.params.epoch.toString());
 
-  if (epoch === null) {
-    epoch = new Epoch(event.params.epoch.toString());
-    epoch.save();
-  }
+    if (epoch === null) {
+      epoch = new Epoch(event.params.epoch.toString());
+      epoch.save();
+    }
 
-  let epochBlock = new EpochBlock(event.params.epoch.toString() + "-" + event.params.networkId.toString());
-  epochBlock.epoch = event.params.epoch.toString();
-  epochBlock.network = event.params.networkId.toString();
-  epochBlock.blockHash = event.params.blockHash;
-  epochBlock.save();
-
-}
+    let epochBlock = new EpochBlock(event.params.epoch.toString() + "-" + event.params.networkId.toString());
+    epochBlock.epoch = event.params.epoch.toString();
+    epochBlock.network = event.params.networkId.toString();
+    epochBlock.blockHash = event.params.blockHash;
+    epochBlock.save();
+    }
 }
