a6f37c74fca428dafd4b23f949f19d7bcfcbd4db
tilacog <tilacog@gmail.com>
2022-04-11 11:54:15 -0300

    inital types layout

--- packages/oracle/src/main.rs
+++ packages/oracle/src/main.rs
@@ -1,8 +1,41 @@
 mod store;
 
+use epoch_encoding;
+use std::collections::HashMap;
+use std::io;
+use store::models::Caip2ChainId;
 pub use store::Store;
 
-use std::io;
+/// Actively listens for new blocks and reorgs from registered blockchains. Also, it checks the
+/// number of confirmations for transactions sent to the DataEdge contract.
+type EventSource = ();
+
+/// Responsible for receiving the encodede payload, constructing and signing the transactions to
+/// Ethereum Mainnet.
+type EthereumClient = ();
+
+/// Encodes and compresses messages.
+type Encoder = ();
+
+/// Tracks current Ethereum mainnet epoch.
+type EpochTracker = ();
+
+// -------------
+
+type BlockChainState = ();
+
+/// The main application in-memory state
+struct Oracle {
+    // -- components --
+    store: Store,
+    event_source: EventSource,
+    encoder: Encoder,
+    ethereum_client: EthereumClient,
+    epoch_tracker: EpochTracker,
+
+    // -- data --
+    state_by_blockchain: HashMap<Caip2ChainId, BlockChainState>,
+}
 
 fn main() -> io::Result<()> {
     Ok(())
