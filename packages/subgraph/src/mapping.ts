d34454433fadad1caca2ad40251c16bd3c8a598e
Filippo Costa <filippo.costa@protonmail.com>
2022-05-16 16:35:09 +0200

    Sync Rust team's branch with the latest subgraph PoC (#41)
    * Added tag decoding and slight refactor
    
    * Added some helpers, test scaffolding and more
    
    * Fixed type system blocking binary operator, tests broken still
    
    * Refactored prefix varint code using ctz
    
    * Refactored tests and decode u64 helper
    
    * Added test case for decode u64 invalid bytes
    
    * Added ZigZag Decoding and basic ZigZag tests
    
    * Made data field optional, added more scaffolding for execute message
    
    * Added functioning register networks and debug logs
    
    * Storing accelerations
    
    * Added Epoch and block numbers processing
    
    Co-authored-by: Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,5 +1,34 @@
 import { CrossChainEpochOracleCall } from "../generated/DataEdge/DataEdge";
-import { DataEdge, Message } from "../generated/schema";
+import { Bytes, BigInt, log } from "@graphprotocol/graph-ts";
+import {
+  DataEdge,
+  SetBlockNumbersForEpochMessage,
+  CorrectEpochsMessage,
+  UpdateVersionsMessage,
+  RegisterNetworksMessage,
+  MessageBlock,
+  Payload,
+  GlobalState,
+  Network
+} from "../generated/schema";
+import {
+  getGlobalState,
+  getTags,
+  decodePrefixVarIntU64,
+  decodePrefixVarIntI64,
+  getStringFromBytes,
+  getAuxGlobalState,
+  commitToGlobalState,
+  rollbackToGlobalState,
+  getOrCreateEpoch,
+  createOrUpdateNetworkEpochBlockNumber
+} from "./helpers";
+import {
+  PREAMBLE_BIT_LENGTH,
+  TAG_BIT_LENGTH,
+  BIGINT_ZERO,
+  BIGINT_ONE
+} from "./constants";
 
 export function handleCrossChainEpochOracle(
   call: CrossChainEpochOracleCall
@@ -9,9 +38,217 @@ export function handleCrossChainEpochOracle(
   let payloadBytes = call.inputs._payload;
   let txHash = call.transaction.hash.toHexString();
 
-  // Save raw message
-  let message = new Message(txHash);
-  message.data = payloadBytes;
-  message.submitter = submitter;
+  // Load auxiliary GlobalState for rollback capabilities
+  let globalState = getAuxGlobalState();
+
+  // Save raw payload
+  let payload = new Payload(txHash);
+  payload.data = payloadBytes;
+  payload.submitter = submitter;
+  payload.save();
+
+  let rawPayloadData = payloadBytes;
+
+  let messageBlockCounter = 0;
+
+  while (rawPayloadData.length > 0) {
+    log.warning("NEW MESSAGE BLOCK {}", [messageBlockCounter.toString()]);
+    // Save raw message
+    let messageBlock = new MessageBlock(
+      [txHash, BigInt.fromI32(messageBlockCounter).toString()].join("-")
+    );
+
+    let tags = getTags(
+      changetype<Bytes>(rawPayloadData.slice(0, PREAMBLE_BIT_LENGTH / 8))
+    );
+
+    rawPayloadData = changetype<Bytes>(
+      rawPayloadData.slice(PREAMBLE_BIT_LENGTH / 8)
+    );
+
+    for (let i = 0; i < tags.length; i++) {
+      log.warning("NEW LOOP", []);
+      log.warning("Payload size now {}", [rawPayloadData.length.toString()]);
+
+      if (rawPayloadData.length == 0) {
+        //rollbackToGlobalState(globalState);
+        //return;
+        break;
+      }
+
+      let bytesRead = executeMessage(
+        tags[i],
+        i,
+        globalState,
+        messageBlock.id,
+        rawPayloadData
+      );
+      rawPayloadData = changetype<Bytes>(rawPayloadData.slice(bytesRead));
+      log.warning("Bytes read: {}", [bytesRead.toString()]);
+    }
+
+    messageBlock.data = rawPayloadData; // cut it to the amount actually read
+    messageBlock.save();
+    log.warning("END OF MESSAGE BLOCK {}", [messageBlockCounter.toString()]);
+    messageBlockCounter++;
+  }
+
+  commitToGlobalState(globalState);
+}
+
+// Executes the message and returns the amount of bytes read
+function executeMessage(
+  tag: i32,
+  index: i32,
+  globalState: GlobalState,
+  messageBlockID: String,
+  data: Bytes
+): i32 {
+  let bytesRead = 0;
+  // ToDo, parse and actually execute message
+  let message = new SetBlockNumbersForEpochMessage(
+    [messageBlockID, BigInt.fromI32(index).toString()].join("-")
+  );
+  message.block = messageBlockID;
+
+  if (tag == 0) {
+    log.warning("EXECUTING SetBlockNumbersForEpochMessage", []);
+    bytesRead = executeSetBlockNumbersForEpochMessage(
+      message,
+      globalState,
+      data
+    );
+  } else if (tag == 1) {
+    log.warning("EXECUTING CorrectEpochsMessage", []);
+    bytesRead = executeCorrectEpochsMessage(
+      changetype<CorrectEpochsMessage>(message),
+      globalState,
+      data
+    );
+  } else if (tag == 2) {
+    log.warning("EXECUTING UpdateVersionsMessage", []);
+    bytesRead = executeUpdateVersionsMessage(
+      changetype<UpdateVersionsMessage>(message),
+      globalState,
+      data
+    );
+  } else if (tag == 3) {
+    log.warning("EXECUTING RegisterNetworksMessage", []);
+    bytesRead = executeRegisterNetworksMessage(
+      changetype<RegisterNetworksMessage>(message),
+      globalState,
+      data
+    );
+  }
+
+  return bytesRead;
+}
+
+function executeSetBlockNumbersForEpochMessage(
+  message: SetBlockNumbersForEpochMessage,
+  globalState: GlobalState,
+  data: Bytes
+): i32 {
+  let bytesRead = 0;
+
+  if (globalState.activeNetworkCount != 0) {
+    let newEpoch = getOrCreateEpoch(
+      (globalState.latestValidEpoch != null
+        ? BigInt.fromString(globalState.latestValidEpoch!)
+        : BIGINT_ZERO) + BIGINT_ONE
+    );
+    globalState.latestValidEpoch = newEpoch.id;
+
+    message.merkleRoot = changetype<Bytes>(
+      data.slice(bytesRead, bytesRead + 32)
+    );
+    bytesRead += 32;
+    let accelerations: Array<BigInt> = [];
+    for (let i = 0; i < globalState.activeNetworkCount; i++) {
+      let readAcceleration = decodePrefixVarIntI64(data, bytesRead); // we should check for errors here
+      bytesRead += readAcceleration[1] as i32;
+      accelerations.push(BigInt.fromI64(readAcceleration[0]));
+
+      // Create new NetworkEpochBlockNumber
+      createOrUpdateNetworkEpochBlockNumber(
+        i.toString(),
+        newEpoch.epochNumber,
+        BigInt.fromI64(readAcceleration[0])
+      );
+    }
+
+    message.accelerations = accelerations;
+    message.data = changetype<Bytes>(data.slice(0, bytesRead));
+    message.save();
+  } else {
+    message.save();
+  }
+  return bytesRead;
+}
+
+function executeCorrectEpochsMessage(
+  message: CorrectEpochsMessage,
+  globalState: GlobalState,
+  data: Bytes
+): i32 {
+  let bytesRead = 0;
+  // To Do
+  return bytesRead;
+}
+
+function executeUpdateVersionsMessage(
+  message: UpdateVersionsMessage,
+  globalState: GlobalState,
+  data: Bytes
+): i32 {
+  let bytesRead = 0;
+  // To Do
+  return bytesRead;
+}
+
+function executeRegisterNetworksMessage(
+  message: RegisterNetworksMessage,
+  globalState: GlobalState,
+  data: Bytes
+): i32 {
+  let bytesRead = 0;
+  // get remove length
+  let readRemoveLength = decodePrefixVarIntU64(data, bytesRead); // we should check errors here
+  bytesRead += readRemoveLength[1] as i32;
+
+  // now get all the removed network ids
+  for (let i = 0; i < (readRemoveLength[0] as i32); i++) {
+    let readRemove = decodePrefixVarIntU64(data, bytesRead); // we should check errors here
+    bytesRead += readRemove[1] as i32;
+    // remove networks to do
+    globalState.activeNetworkCount -= 1;
+  }
+
+  let readAddLength = decodePrefixVarIntU64(data, bytesRead); // we should check errors here
+  bytesRead += readAddLength[1] as i32;
+
+  // now get all the add network strings
+  for (let i = 0; i < (readAddLength[0] as i32); i++) {
+    let readStrLength = decodePrefixVarIntU64(data, bytesRead); // we should check errors here
+    bytesRead += readStrLength[1] as i32;
+
+    let chainID = getStringFromBytes(data, bytesRead, readStrLength[0] as u32);
+    bytesRead += readStrLength[0] as i32;
+
+    // ID used here might need to change depending on what we get passed in the removes
+    let network = new Network(globalState.networkCount.toString());
+    network.chainID = chainID;
+    network.addedAt = message.id;
+    network.save();
+
+    globalState.networkCount += 1;
+    globalState.activeNetworkCount += 1;
+  }
+
+  message.data = changetype<Bytes>(data.slice(0, bytesRead));
+  message.removeCount = BigInt.fromU64(readRemoveLength[0]);
+  message.addCount = BigInt.fromU64(readAddLength[0]);
   message.save();
+
+  return bytesRead;
 }
---
+++ packages/subgraph/tests/decoding.test.ts
@@ -0,0 +1,193 @@
+import { clearStore, test, assert } from "matchstick-as/assembly/index";
+import { handleCrossChainEpochOracle } from "../src/mapping";
+import {
+  getGlobalState,
+  getTags,
+  decodePrefixVarIntU64,
+  decodePrefixVarIntI64,
+  zigZagDecode
+} from "../src/helpers";
+import { log } from "@graphprotocol/graph-ts";
+
+import { CrossChainEpochOracleCall } from "../generated/DataEdge/DataEdge";
+import { Bytes, BigInt } from "@graphprotocol/graph-ts";
+import { DataEdge, MessageBlock, Payload } from "../generated/schema";
+
+test("U64 Decoding 0x2F", () => {
+  // 23 -> [47] -> 0x2F
+  let encoded = Bytes.fromHexString("0x2F"); // 23 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(BigInt.fromU64(decoded[0]), BigInt.fromU64(23 as u64));
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(1 as u64));
+});
+
+test("U64 Decoding 0xA28C", () => {
+  // 9000 -> [162, 140] -> 0xA28C
+  let encoded = Bytes.fromHexString("0xA28C"); // 9000 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(BigInt.fromU64(decoded[0]), BigInt.fromU64(9000 as u64));
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(2 as u64));
+});
+
+test("U64 Decoding 0x54AFB1", () => {
+  // 1455594 -> [84, 175, 177] -> 0x54AFB1
+  let encoded = Bytes.fromHexString("0x54AFB1"); // 1455594 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(1455594 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(3 as u64));
+});
+
+test("U64 Decoding 0x58B1AF68", () => {
+  // 109771541 -> [88, 177, 175, 104] -> 0x58B1AF68
+  let encoded = Bytes.fromHexString("0x58B1AF68"); // 109771541 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(109771541 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(4 as u64));
+});
+
+test("U64 Decoding 0xF0DF2264B5", () => {
+  // 24345908991 -> [240, 223, 34, 100, 181] -> 0xF0DF2264B5
+  let encoded = Bytes.fromHexString("0xF0DF2264B5"); // 24345908991 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(24345908991 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(5 as u64));
+});
+
+test("U64 Decoding 0x608FF0EBC86E", () => {
+  // 1903269233213 -> [96, 143, 240, 235, 200, 110] -> 0x608FF0EBC86E
+  let encoded = Bytes.fromHexString("0x608FF0EBC86E"); // 1903269233213 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(1903269233213 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(6 as u64));
+});
+
+test("U64 Decoding 0x40FD170EAC2DFE", () => {
+  // 558944227176442 -> [64, 253, 23, 14, 172, 45, 254] -> 0x40FD170EAC2DFE
+  let encoded = Bytes.fromHexString("0x40FD170EAC2DFE"); // 558944227176442 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(558944227176442 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(7 as u64));
+});
+
+test("U64 Decoding 0x80FFFFFFFFFFFFFF", () => {
+  // 72057594037927935 -> [128, 255, 255, 255, 255, 255, 255, 255] -> 0x80FFFFFFFFFFFFFF
+  let encoded = Bytes.fromHexString("0x80FFFFFFFFFFFFFF"); // 72057594037927935 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(72057594037927935 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(8 as u64));
+});
+
+test("U64 Decoding 0x00FFFFFFFFFFFFFFFF", () => {
+  // 18446744073709551615 -> [0, 255, 255, 255, 255, 255, 255, 255, 255] 0x00FFFFFFFFFFFFFFFF
+  let encoded = Bytes.fromHexString("0x00FFFFFFFFFFFFFFFF"); // 18446744073709551615 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value
+  assert.bigIntEquals(
+    BigInt.fromU64(decoded[0]),
+    BigInt.fromU64(18446744073709551615 as u64)
+  );
+  // Assert length of bytes read
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(9 as u64));
+});
+
+test("U64 Decoding InvalidFormat 0x0011", () => {
+  // 18446744073709551615 -> [0, 255, 255, 255, 255, 255, 255, 255, 255] 0x00FFFFFFFFFFFFFFFF
+  let encoded = Bytes.fromHexString("0x0011"); // 18446744073709551615 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value (0 since invalid cases return 0 here)
+  assert.bigIntEquals(BigInt.fromU64(decoded[0]), BigInt.fromU64(0 as u64));
+  // Assert length of bytes read (0 since ivalid cases return 0 here.)
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(0 as u64));
+});
+
+test("U64 Decoding InvalidFormat 0x00", () => {
+  // 18446744073709551615 -> [0, 255, 255, 255, 255, 255, 255, 255, 255] 0x00FFFFFFFFFFFFFFFF
+  let encoded = Bytes.fromHexString("0x00"); // 18446744073709551615 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value (0 since invalid cases return 0 here)
+  assert.bigIntEquals(BigInt.fromU64(decoded[0]), BigInt.fromU64(0 as u64));
+  // Assert length of bytes read (0 since ivalid cases return 0 here.)
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(0 as u64));
+});
+
+test("U64 Decoding InvalidFormat 0x00FFFFFFFFFFFFFF (1 byte less than valid)", () => {
+  // 18446744073709551615 -> [0, 255, 255, 255, 255, 255, 255, 255, 255] 0x00FFFFFFFFFFFFFFFF
+  let encoded = Bytes.fromHexString("0x00FFFFFFFFFFFFFF"); // 18446744073709551615 u64
+  let decoded = decodePrefixVarIntU64(changetype<Bytes>(encoded), 0);
+  // Assert decoded value (0 since invalid cases return 0 here)
+  assert.bigIntEquals(BigInt.fromU64(decoded[0]), BigInt.fromU64(0 as u64));
+  // Assert length of bytes read (0 since ivalid cases return 0 here.)
+  assert.bigIntEquals(BigInt.fromU64(decoded[1]), BigInt.fromU64(0 as u64));
+});
+
+test("ZigZag Decoding 0", () => {
+  // 0 should decode to 0
+  let encoded = 0 as u64;
+  let decoded = zigZagDecode(encoded);
+
+  assert.bigIntEquals(BigInt.fromI64(decoded), BigInt.fromI64(0 as i64));
+});
+
+test("ZigZag Decoding 1", () => {
+  // 1 should decode to -1
+  let encoded = 1 as u64;
+  let decoded = zigZagDecode(encoded);
+
+  assert.bigIntEquals(BigInt.fromI64(decoded), BigInt.fromI64(-1 as i64));
+});
+
+test("ZigZag Decoding 4294967294", () => {
+  // 4294967294 should decode to 2147483647
+  let encoded = 4294967294 as u64;
+  let decoded = zigZagDecode(encoded);
+
+  assert.bigIntEquals(
+    BigInt.fromI64(decoded),
+    BigInt.fromI64(2147483647 as i64)
+  );
+});
+
+test("ZigZag Decoding 4294967295", () => {
+  // 4294967295 should decode to -2147483648
+  let encoded = 4294967295 as u64;
+  let decoded = zigZagDecode(encoded);
+
+  assert.bigIntEquals(
+    BigInt.fromI64(decoded),
+    BigInt.fromI64(-2147483648 as i64)
+  );
+});
