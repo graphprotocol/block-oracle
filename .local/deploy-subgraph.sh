260ce5dc61eae00b98ce289dd7dceec3e10f338b
tilacog <tilacog@gmail.com>
2022-05-17 21:21:04 -0300

    Fix curl command to check graph-node readiness

--- .local/deploy-subgraph.sh
+++ .local/deploy-subgraph.sh
@@ -9,7 +9,7 @@ yarn
 yarn codegen
 yarn build
 
-await "curl --silent --fail --request OPTIONS localhost:${GRAPH_NODE_JRPC_PORT}"
+await "curl --silent --fail localhost:${GRAPH_NODE_JRPC_PORT}"
 
 yarn create-hardhat
 yarn deploy-hardhat
