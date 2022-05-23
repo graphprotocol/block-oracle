f7062bc637d7e6a40d47ff680df04049da446541
Filippo Costa <filippo@neysofu.me>
2022-05-23 11:40:19 +0200

    fix: update preamble tag bit length 2 -> 4

--- packages/subgraph/src/constants.ts
+++ packages/subgraph/src/constants.ts
@@ -1,6 +1,7 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 export const PREAMBLE_BIT_LENGTH = 8;
-export const TAG_BIT_LENGTH = 2;
+export const TAG_BIT_LENGTH = 4;
+
 export let BIGINT_ZERO = BigInt.fromI32(0);
 export let BIGINT_ONE = BigInt.fromI32(1);
