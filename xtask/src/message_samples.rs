0e05af998da55faf1cd7327ad0d1087caae49498
tilacog <tilacog@pop-os.localdomain>
2022-06-08 17:24:33 -0700

    fix wrong json path placement, use more explicit paths

--- xtask/src/message_samples.rs
+++ xtask/src/message_samples.rs
@@ -1,19 +1,20 @@
 use anyhow::Context;
 use glob::glob;
-use std::fs::write;
+use std::{fs::write, path::PathBuf};
 use xshell::{cmd, Shell};
 
-const SAMPLES_DIRECTORY: &'static str = "crates/oracle-encoder/examples/jsonnet-examples";
+const JSONNET_SAMPLES_DIRECTORY: &'static str = "crates/oracle-encoder/examples/jsonnet-examples";
+const JSON_SAMPLES_DIRECTORY: &'static str = "crates/oracle-encoder/examples/";
 
 fn compile() -> anyhow::Result<()> {
     let sh = Shell::new()?;
-    for jsonnet_file in glob(&format!("{}/*.jsonnet", SAMPLES_DIRECTORY))? {
+    for jsonnet_file in glob(&format!("{}/*.jsonnet", JSONNET_SAMPLES_DIRECTORY))? {
         let jsonnet_path = jsonnet_file?;
         let json = cmd!(sh, "jsonnet {jsonnet_path}")
             .read()
             .context("jsonnet failed")?;
         let target_file_name = {
-            let mut base_path = jsonnet_path.parent().unwrap().parent().unwrap().to_owned();
+            let mut base_path = PathBuf::from(JSON_SAMPLES_DIRECTORY);
             let jsonnet_file_name = jsonnet_path.file_name().unwrap().to_string_lossy();
             let json_file_name = jsonnet_file_name.trim_end_matches("net");
             base_path.push(json_file_name);
@@ -30,7 +31,8 @@ pub fn encode() -> anyhow::Result<()> {
     cmd!(sh, "cargo build --package oracle-encoder")
         .quiet()
         .run()?;
-    for json_file in glob(&format!("{}/*.json", SAMPLES_DIRECTORY))? {
+
+    for json_file in glob(&format!("{}/*.json", JSON_SAMPLES_DIRECTORY))? {
         let json_path = json_file?;
         let output = cmd!(sh, "./target/debug/oracle-encoder --json-path {json_path}").read()?;
         let file_name = json_path.to_string_lossy();
