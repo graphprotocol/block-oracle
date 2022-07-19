cef3341852a62a816d8d02b719f88b6d3e972336
Filippo Costa <filippocosta.italy@gmail.com>
2022-07-19 14:20:02 +0200

    Unify options availabile under CLI and TOML configuration file (#140)
    * feat: unify Clap and TOML options; only keep config.toml
    
    * fix: set env var to pass unit test

--- .local/block-oracle.sh
+++ .local/block-oracle.sh
@@ -10,11 +10,7 @@ cargo build
 await_contract
 await_subgraph
 
-./target/debug/block-oracle \
-	--config-file=./crates/oracle/config/dev/config.toml \
-	--subgraph-url="http://127.0.0.1:${GRAPH_NODE_GRAPHQL_PORT}/subgraphs/name/edgeandnode/block-oracle" \
-	--owner-private-key=4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d || true
-
+./target/debug/block-oracle ./crates/oracle/config/dev/config.toml || true
 
 echo
 echo "Epoch Block Oracle crashed."
--- crates/oracle/src/config.rs
+++ crates/oracle/src/config.rs
@@ -3,8 +3,10 @@ use anyhow::Context;
 use clap::Parser;
 use secp256k1::SecretKey;
 use serde::Deserialize;
+use serde_utils::{EitherLiteralOrEnvVar, FromStrWrapper};
 use std::{
     collections::HashMap,
+    fmt::Display,
     fs::read_to_string,
     path::{Path, PathBuf},
     str::FromStr,
@@ -24,6 +26,19 @@ pub enum ConfigError {
 }
 
 #[derive(Clone, Debug)]
+pub struct IndexedChain {
+    pub id: Caip2ChainId,
+    pub jrpc_url: Url,
+}
+
+#[derive(Clone, Debug)]
+pub struct ProtocolChain {
+    pub id: Caip2ChainId,
+    pub jrpc_url: Url,
+    pub polling_interval: Duration,
+}
+
+#[derive(Clone, Debug)]
 pub struct Config {
     pub log_level: LevelFilter,
     pub owner_private_key: SecretKey,
@@ -37,19 +52,6 @@ pub struct Config {
     pub retry_strategy_max_wait_time: Duration,
 }
 
-#[derive(Clone, Debug)]
-pub struct IndexedChain {
-    pub id: Caip2ChainId,
-    pub jrpc_url: Url,
-}
-
-#[derive(Clone, Debug)]
-pub struct ProtocolChain {
-    pub id: Caip2ChainId,
-    pub jrpc_url: Url,
-    pub polling_interval: Duration,
-}
-
 impl Config {
     /// Loads all configuration options from CLI arguments, the TOML
     /// configuration file, and environment variables.
@@ -59,57 +61,45 @@ impl Config {
             .context("Failed to read config file as valid TOML")
             .unwrap();
 
-        Self::from_clap_and_config_file(clap, config_file)
+        Self::from_config_file(config_file)
     }
 
     #[cfg(test)]
     fn parse_from(args: &[&str]) -> Self {
         let clap = Clap::parse_from(args);
-        println!("clap config is {:?}", clap.config_file);
         let config_file = ConfigFile::from_file(&clap.config_file).unwrap();
 
-        Self::from_clap_and_config_file(clap, config_file)
+        Self::from_config_file(config_file)
     }
 
-    fn from_clap_and_config_file(clap: Clap, config_file: ConfigFile) -> Self {
-        let retry_strategy_max_wait_time =
-            Duration::from_secs(config_file.web3_transport_retry_max_wait_time_in_seconds);
-
+    fn from_config_file(config_file: ConfigFile) -> Self {
         Self {
-            log_level: clap.log_level,
-            owner_private_key: SecretKey::from_str(clap.owner_private_key.as_str()).unwrap(),
-            contract_address: config_file.contract_address.parse().unwrap(),
-            subgraph_url: clap.subgraph_url,
+            log_level: config_file.log_level.0,
+            owner_private_key: config_file.owner_private_key.0,
+            contract_address: config_file.contract_address.0,
+            subgraph_url: config_file.subgraph_url.0,
             freshness_threshold: config_file.freshness_threshold,
             epoch_duration: config_file.epoch_duration,
-            owner_address: config_file.owner_address.parse().unwrap(),
-            retry_strategy_max_wait_time,
+            owner_address: config_file.owner_address.0,
+            retry_strategy_max_wait_time: Duration::from_secs(
+                config_file.web3_transport_retry_max_wait_time_in_seconds,
+            ),
             indexed_chains: config_file
                 .indexed_chains
                 .into_iter()
                 .map(|(id, provider)| IndexedChain {
                     id,
-                    jrpc_url: parse_jrpc_provider_url(&provider)
-                        .expect("Bad JSON-RPC provider url"),
+                    jrpc_url: provider.0,
                 })
                 .collect::<Vec<IndexedChain>>(),
             protocol_chain: ProtocolChain {
                 id: config_file.protocol_chain.name,
-                jrpc_url: parse_jrpc_provider_url(&config_file.protocol_chain.jrpc)
-                    .expect("Invalid protocol chain JSON-RPC provider url"),
+                jrpc_url: config_file.protocol_chain.jrpc.0,
                 polling_interval: Duration::from_secs(
-                    config_file.protocol_chain_polling_interval_in_seconds,
+                    config_file.protocol_chain.polling_interval_in_seconds,
                 ),
             },
         }
-    }
-}
-
-fn parse_jrpc_provider_url(s: &str) -> anyhow::Result<Url> {
-    if let Ok(url) = Url::parse(s) {
-        Ok(url)
-    } else {
-        Ok(Url::parse(std::env::var(s)?.as_str())?)
     }
 }
 
@@ -118,17 +108,8 @@ fn parse_jrpc_provider_url(s: &str) -> anyhow::Result<Url> {
 #[clap(bin_name = "block-oracle")]
 #[clap(author, version, about, long_about = None)]
 struct Clap {
-    /// The private key for the oracle owner account.
-    #[clap(long)]
-    owner_private_key: String,
-    /// Only show log messages at or above this level. `INFO` by default.
-    #[clap(short, long, default_value = "info")]
-    log_level: LevelFilter,
-    /// The subgraph endpoint.
-    #[clap(long)]
-    subgraph_url: Url,
-    /// The filepath of the TOML JSON-RPC configuration file.
-    #[clap(long, parse(from_os_str))]
+    /// The filepath of the TOML configuration file.
+    #[clap(parse(from_os_str))]
     config_file: PathBuf,
 }
 
@@ -136,10 +117,10 @@ struct Clap {
 #[derive(Deserialize)]
 #[serde(rename_all = "snake_case")]
 struct ConfigFile {
-    owner_address: String,
-    contract_address: String,
-    indexed_chains: HashMap<Caip2ChainId, String>,
-    protocol_chain: SerdeProtocolChain,
+    owner_address: FromStrWrapper<H160>,
+    owner_private_key: EitherLiteralOrEnvVar<SecretKey>,
+    contract_address: FromStrWrapper<H160>,
+    subgraph_url: EitherLiteralOrEnvVar<Url>,
     /// Number of blocks that the Epoch Subgraph may be away from the protocol chain's head. If the
     /// block distance is lower than this, a `trace_filter` JSON RPC call will be used to infer if
     /// any relevant transaction happened within that treshold.
@@ -147,14 +128,16 @@ struct ConfigFile {
     freshness_threshold: u64,
     #[serde(default = "serde_defaults::epoch_duration")]
     epoch_duration: u64,
-    #[serde(default = "serde_defaults::protocol_chain_polling_interval_in_seconds")]
-    protocol_chain_polling_interval_in_seconds: u64,
     #[serde(default = "serde_defaults::web3_transport_retry_max_wait_time_in_seconds")]
     web3_transport_retry_max_wait_time_in_seconds: u64,
     #[serde(default = "serde_defaults::transaction_confirmation_poll_interval_in_seconds")]
     transaction_confirmation_poll_interval_in_seconds: u64,
     #[serde(default = "serde_defaults::transaction_confirmation_count")]
     transaction_confirmation_count: usize,
+    #[serde(default = "serde_defaults::log_level")]
+    log_level: FromStrWrapper<LevelFilter>,
+    protocol_chain: SerdeProtocolChain,
+    indexed_chains: HashMap<Caip2ChainId, EitherLiteralOrEnvVar<Url>>,
 }
 
 impl ConfigFile {
@@ -165,10 +148,71 @@ impl ConfigFile {
     }
 }
 
+#[derive(Deserialize, Debug)]
+struct SerdeProtocolChain {
+    name: Caip2ChainId,
+    jrpc: EitherLiteralOrEnvVar<Url>,
+    #[serde(default = "serde_defaults::protocol_chain_polling_interval_in_seconds")]
+    polling_interval_in_seconds: u64,
+}
+
+mod serde_utils {
+    use super::*;
+
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub struct EitherLiteralOrEnvVar<T>(pub T);
+
+    impl<'de, T> serde::Deserialize<'de> for EitherLiteralOrEnvVar<T>
+    where
+        T: FromStr,
+        T::Err: Display,
+    {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            let raw_s = String::deserialize(deserializer)?;
+            let value_s = if raw_s.starts_with('$') {
+                std::env::var(raw_s.strip_prefix('$').unwrap()).map_err(serde::de::Error::custom)?
+            } else {
+                raw_s
+            };
+            Ok(EitherLiteralOrEnvVar(
+                T::from_str(&value_s).map_err(serde::de::Error::custom)?,
+            ))
+        }
+    }
+
+    pub struct FromStrWrapper<T>(pub T);
+
+    impl<'de, T> Deserialize<'de> for FromStrWrapper<T>
+    where
+        T: FromStr,
+        T::Err: Display,
+    {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            let s = String::deserialize(deserializer)?;
+            FromStr::from_str(&s)
+                .map(Self)
+                .map_err(serde::de::Error::custom)
+        }
+    }
+}
+
 /// These should be expressed as constants once
 /// https://github.com/serde-rs/serde/issues/368 is fixed.
 #[allow(unused)]
 mod serde_defaults {
+    use super::serde_utils::FromStrWrapper;
+    use tracing_subscriber::filter::LevelFilter;
+
+    pub fn log_level() -> FromStrWrapper<LevelFilter> {
+        FromStrWrapper(LevelFilter::INFO)
+    }
+
     pub fn freshness_threshold() -> u64 {
         10
     }
@@ -194,12 +238,6 @@ mod serde_defaults {
     }
 }
 
-#[derive(Deserialize, Debug)]
-struct SerdeProtocolChain {
-    name: Caip2ChainId,
-    jrpc: String,
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -213,12 +251,8 @@ mod tests {
             .clone()
     }
 
-    fn config_file_flag(filename: &str) -> String {
-        format!(
-            "--config-file={}/config/{}",
-            env!("CARGO_MANIFEST_DIR"),
-            filename
-        )
+    fn config_file_path(filename: &str) -> String {
+        format!("{}/config/{}", env!("CARGO_MANIFEST_DIR"), filename)
     }
 
     #[test]
@@ -226,20 +260,14 @@ mod tests {
     fn invalid_jrpc_provider_url() {
         Config::parse_from(&[
             "",
-            "--subgraph-url=https://example.com",
-            "--owner-private-key=4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
-            config_file_flag("test/invalid_jrpc_provider_url.toml").as_str(),
+            config_file_path("test/invalid_jrpc_provider_url.toml").as_str(),
         ]);
     }
 
     #[test]
     fn example_config() {
-        Config::parse_from(&[
-            "",
-            "--subgraph-url=https://example.com",
-            "--owner-private-key=4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
-            config_file_flag("dev/config.toml").as_str(),
-        ]);
+        std::env::set_var("SUBGRAPH_URL", "https://example.com");
+        Config::parse_from(&["", config_file_path("dev/config.toml").as_str()]);
     }
 
     #[test]
@@ -249,9 +277,7 @@ mod tests {
 
         let config = Config::parse_from(&[
             "",
-            "--subgraph-url=https://example.com",
-            "--owner-private-key=4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
-            config_file_flag("test/indexed_chain_provider_via_env_var.toml").as_str(),
+            config_file_path("test/indexed_chain_provider_via_env_var.toml").as_str(),
         ]);
 
         assert_eq!(
