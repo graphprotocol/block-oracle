fc20683149f3d14df683755260f5bf25c486a167
Filippo Costa <filippocosta.italy@gmail.com>
2022-07-19 14:07:28 +0200

    feat: unify Clap and TOML options; only keep config.toml

--- crates/oracle/config/dev/block-oracle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd ../../packages/contracts
-yarn hardhat run --network localhost scripts/deploy.ts
-cd ../../crates/oracle
-
-cargo run -- \
-	--config-file=config/dev/config.toml \
-	--owner-private-key=4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d
