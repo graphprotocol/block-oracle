ef8160388dea955264f2760f45092902006ba2d4
tilacog <tilacog@gmail.com>
2022-07-28 14:07:56 -0300

    refactor: more convenient run script for the local environment

--- .local/run.sh
+++ .local/run.sh
@@ -1,1 +1,10 @@
-overmind start --daemonize && sleep 2 && overmind connect "graph-node"
+#!/usr/bin/env bash
+set -eu
+
+PROCESS_NAME=${1:-block-oracle}
+
+rm .overmind.sock || true
+
+overmind start --daemonize
+sleep 2
+overmind connect "$PROCESS_NAME"
