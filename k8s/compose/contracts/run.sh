d899a43956004f64ffd8a59c4f58ac878a2305a1
tilacog <tilacog@gmail.com>
2022-08-15 20:08:59 -0300

    fix: remove redundant `yarn build` step

--- k8s/compose/contracts/run.sh
+++ k8s/compose/contracts/run.sh
@@ -6,7 +6,6 @@ yarn hardhat run --network compose scripts/deploy-local.ts
 
 # deploy the network contracts (including the Epoch Manager)
 cd network-contracts
-yarn build
 yarn hardhat migrate --config extended.config.ts --network compose --graph-config config/graph.localhost.yml --skip-confirmation
 
 # set automining interval
