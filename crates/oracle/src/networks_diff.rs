f5e59d5868b34fc6c2745160e8c7bccc8d62d59d
tilacog <tilacog@gmail.com>
2022-08-17 11:29:48 -0300

    fix: reword ID -> index

--- crates/oracle/src/networks_diff.rs
+++ crates/oracle/src/networks_diff.rs
@@ -15,11 +15,11 @@ impl NetworksDiff {
 
     fn diff(old: &[Network], new: HashSet<Caip2ChainId>) -> Self {
         let mut deletions = HashMap::new();
-        let mut deleted_ids = HashSet::new();
+        let mut deleted_indices = HashSet::new();
         for network in old.iter() {
             if !new.contains(&network.id) {
                 deletions.insert(network.id.clone(), network.array_index);
-                deleted_ids.insert(network.array_index);
+                deleted_indices.insert(network.array_index);
             }
         }
 
@@ -27,27 +27,27 @@ impl NetworksDiff {
         let old_network_names: HashSet<_> = old.iter().map(|network| &network.id).collect();
         for network_name in new.into_iter() {
             if !old_network_names.contains(&network_name) {
-                // We use 0 as a temporary ID.
+                // We use 0 as a temporary index.
                 insertions.insert(network_name, 0);
             }
         }
 
-        // Now we can assign IDs to the newly-inserted networks. We want to
-        // recycle IDs as much as possible, so the lowest nonnegative number
+        // Now we can assign indices to the newly-inserted networks. We want to
+        // recycle indices as much as possible, so the lowest nonnegative number
         // that's not currently assigned must be used.
-        let mut next_candidate_id = 0;
-        for (_, id) in insertions.iter_mut() {
+        let mut next_candidate_index = 0;
+        for (_, index) in insertions.iter_mut() {
             loop {
-                let id_is_free_to_use = deleted_ids.contains(&next_candidate_id)
+                let index_is_free_to_use = deleted_indices.contains(&next_candidate_index)
                     || !old
                         .iter()
-                        .any(|network| network.array_index == next_candidate_id);
+                        .any(|network| network.array_index == next_candidate_index);
 
-                if id_is_free_to_use {
-                    *id = next_candidate_id;
+                if index_is_free_to_use {
+                    *index = next_candidate_index;
                     break;
                 }
-                next_candidate_id += 1;
+                next_candidate_index += 1;
             }
         }
 
