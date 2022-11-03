8886310a5105ab412857aeb2f4f37616b3011ef2
tilacog <tilacog@gmail.com>
2022-11-02 21:12:05 -0300

    refactor: local environment - use hardhat as an indexed chain
    this removes the dependency on external providers

--- k8s/compose/contracts/seed-then-set-automining-interval.js
+++ k8s/compose/contracts/seed-then-set-automining-interval.js
@@ -1,15 +1,20 @@
 async function main() {
-	await network.provider.send('eth_sendTransaction', [{
-		from: '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1',
-		to: '0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab',
-		data: '0xa1dce33200000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000017030105136569703135353a3737136569703135353a3939000000000000000000'
-	}]);
-	await network.provider.send('evm_setIntervalMining', [2000]);
+  // RegisterNetworks message: add network eip155:1337 (same as protocol chain)
+  // This is necessary because since commit #cc729541e5d3fe0a11ef7f9a4382dd693525eb9e the Epoch Block Oracle won't send the RegisterNetworks message.
+  await network.provider.send("eth_sendTransaction", [
+    {
+      from: "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
+      to: "0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab",
+      data: "0xa1dce3320000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000f030103176569703135353a313333370000000000000000000000000000000000",
+    },
+  ]);
+  //
+  await network.provider.send("evm_setIntervalMining", [2000]);
 }
 
 main()
-	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error(error)
-		process.exit(1)
-	})
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
