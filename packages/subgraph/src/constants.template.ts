9dcc3dd92138afa043f7c1b45165f4f2cfd9f448
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-03-02 20:24:37 -0300

    feat: add revokable permission entries

--- packages/subgraph/src/constants.template.ts
+++ packages/subgraph/src/constants.template.ts
@@ -6,9 +6,9 @@ export const TAG_BIT_LENGTH = 4;
 //export const OWNER_ADDRESS_STRING = "{{owner}}";
 export const EPOCH_MANAGER_ADDRESS = "{{epochManager}}";
 
-export let INITIAL_PERMISSION_SET = new Map<String,Array<String>>();
+export let INITIAL_PERMISSION_SET = new Map<String,Array<Array<String>>>();
 {{#permissionList}}
-INITIAL_PERMISSION_SET.set("{{address}}", [{{#permissions}}"{{entry}}"{{^lastEntry}},{{/lastEntry}}{{/permissions}}])
+INITIAL_PERMISSION_SET.set("{{address}}", [["{{validThrough}}"],[{{#permissions}}"{{entry}}"{{^lastEntry}},{{/lastEntry}}{{/permissions}}]])
 {{/permissionList}}
 export let BIGINT_ZERO = BigInt.fromI32(0);
 export let BIGINT_ONE = BigInt.fromI32(1);
